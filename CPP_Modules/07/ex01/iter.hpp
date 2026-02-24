cc09bfd3e5427b26e8d1e897e926499146411a1d
Niklas Raatikainen <nraatika@student.hive.fi>
2026-02-24 14:29:57 +0200

    improved test on CPP07/ex01

--- CPP_Modules/07/ex01/iter.hpp
+++ CPP_Modules/07/ex01/iter.hpp
@@ -1,23 +1,10 @@
 #ifndef ITER_HPP
 #define ITER_HPP
-
-//funcpointer is allowed to modify array elements
-template <typename T> void iter(T *array, size_t const length, void (*funcPointer)(T&)){
-	for (size_t i = 0; i < length; i++){
-		funcPointer(array[i]);
-	}
-}
-
-//funcpointer is not allowed to modify array elements
-template <typename T> void iter(T const* array, size_t const length, void (*funcPointer)(T const&)){
-	for (size_t i = 0; i < length; i++){
-		funcPointer(array[i]);
-	}
-}
+#include <cstddef>
 
 //this automatically checks that constness of 'function' doesn't downgrade constness of 'array'
 //also allows for function with a return type, even though we discard it.
-template <typename T, typename F> void betterIter(T *array, size_t const length, F function){
+template <typename T, typename F> void iter(T *array, size_t const length, F function){
 	for (size_t i = 0; i < length; i++){
 		function(array[i]);
 	}
--- CPP_Modules/07/ex01/main.cpp
+++ CPP_Modules/07/ex01/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "iter.hpp"
 #define COLOR_GREEN "\033[32m"
-#define COLOR_RESET "\033[0m" 
+#define COLOR_RESET "\033[0m"
+
+template <typename T> void printOfType(T &element){
+    std::cout << "Element: " << element << std::endl;
+};
 
 void addOne(int &input){
 	std::cout << ++input <<std::endl;
@@ -15,6 +19,7 @@ void addOneConst(int const& input){
 int main(){
 	int array[] = {1,2,3,42};
 	int const constArray[] = {1,2,3,24};
+	std::string const strArray[] = {"Hello", "World"};
 
 	std::cout << COLOR_GREEN << "non-const version :" << COLOR_RESET << std::endl;
 	iter(array, 4, &addOne);
@@ -28,4 +33,21 @@ int main(){
 	std::cout << std::endl << COLOR_GREEN << "const version, run a second time:" << COLOR_RESET << std::endl;
 	iter(constArray, 4, &addOneConst);
 
+	std::cout << std::endl << COLOR_GREEN << "Templated function run on every array:" << COLOR_RESET << std::endl;
+	iter(array, 4, &printOfType<int const>);  //this one compiles: you can treat a non-const int & as const if you want
+	iter(constArray, 4, &printOfType<int const>);
+	//iter(constArray, 4, &printOfType<int>); //this won't compile: trying to pass a const int & to something that expects int&
+	iter(strArray, 2, &printOfType<std::string const>);
+
+	std::cout << std::endl << COLOR_GREEN << "Lambda function run on string array:" << COLOR_RESET << std::endl;
+	//lambda functions are anonymous, local functions, syntax: [outside vars passed in] (params) {function body} => returns a "function pointer"
+	// here, let's print out at most 2 character of every string
+	size_t length = 3;
+	iter(strArray, 2, [length] (std::string const &s) {
+	    if(length >= s.length()){
+    		std::cout<< s << std::endl;
+		}else {
+		    std::cout << s.substr(0, length) << std::endl;
+		}
+	});
 }
